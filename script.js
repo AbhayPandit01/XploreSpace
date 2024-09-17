e0b853b599d118fe921ff7eaaab9b7bb2daf884b
AbhayPandit01 <panditabhay2005@gmail.com>
2024-09-17 20:30:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "300px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+};
+
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
